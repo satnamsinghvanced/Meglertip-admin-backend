2d920fb9280951d5ea4835a95352da4b81cb2de4
satnamsinghvanced <145443805+satnamsinghvanced@users.noreply.github.com>
2025-10-29 11:28:38 +0530

    Created controllers cities and companies

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,14 +1,18 @@
-const express = require("express")
+const express = require("express");
 
-const adminRoutes = require('./admin/route');
+const adminRoutes = require("./admin/route");
 const imageUpload = require("./upload/route");
 const faqRoutes = require("./faq/route");
-const categoryRoutes = require("./category/route")
+const categoryRoutes = require("./category/route");
+const cityRotes = require("./city/route");
+const companiesRoutes = require("./companies/route");
 
-const router = express.Router()
-router.use('/admin', adminRoutes);
-router.use("/image" , imageUpload)
-router.use("/faq", faqRoutes)
-router.use("/category", categoryRoutes)
+const router = express.Router();
+router.use("/admin", adminRoutes);
+router.use("/image", imageUpload);
+router.use("/faq", faqRoutes);
+router.use("/category", categoryRoutes);
+router.use("/companies", companiesRoutes);
+router.use("/cities", cityRotes);
 
 module.exports = router;
