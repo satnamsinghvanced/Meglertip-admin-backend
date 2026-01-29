ac1688e96fdd61c3cd07d0374db23a427ac58ee7
Satnam <satnamsingh@vancedsolutions.com>
2026-01-29 12:05:40 +0530

    solve lead issue

--- src/api/leadLogs/route.js
+++ src/api/leadLogs/route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllLeads,getLeadById,updateLeadStatus,updateLeadProfit } = require("./controller");
+const { getAllLeads,getLeadById,updateLeadStatus,updateLeadProfit,getPartnerLeadInvoiceSummary } = require("./controller");
 
 const router = express.Router();
 
@@ -7,6 +7,6 @@ router.get("/all", getAllLeads);
 router.get("/details/:id", getLeadById);
 router.patch("/status", updateLeadStatus);
 router.patch("/update-profit", updateLeadProfit);
-
+router.get("/partner-summary", getPartnerLeadInvoiceSummary);
 
 module.exports = router;
